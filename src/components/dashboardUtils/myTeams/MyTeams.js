1d91e0033ef4bf72589ebfdfc9c32a0ba8cfe077
Prernabudh <prernabudhraja8@gmail.com>
2020-08-05 00:42:53 +0530

    create/join team

--- src/components/dashboardUtils/myTeams/MyTeams.js
+++ src/components/dashboardUtils/myTeams/MyTeams.js
@@ -1,6 +1,8 @@
 import React from "react";
 import SimpleCard from "../myTeams/myTeamCard/MyTeamCard";
 import MyInformation from "../myTeams/myInformation/MyInformation";
+import AddTeamCard from "../myTeams/addTeamCard/AddTeamCard";
+import AddTeamPopup from "../myTeams/addTeamPopup/AddTeamPopup";
 import "./MyTeams.css";
 
 const teams = [
@@ -14,20 +16,30 @@ const teams = [
   },
   {
     id: "3214",
-    name: "Bangalore trip"
+    name: "Bangalore trip",
   },
   {
-    id:"12343",
-    name: "Roommates"
-  }
+    id: "12343",
+    name: "Roommates",
+  },
 ];
 
 const MyTeams = () => {
+  const [showCard, setShowCard] = React.useState(false);
+  const handleAddCard = () => {
+    setShowCard(!showCard);
+  };
   return (
     <div>
       <h1 style={{ marginTop: "20px" }}>My Teams</h1>
+      <div style={{ display: showCard ? "block" : "none" }}>
+        <AddTeamPopup setShowCard={setShowCard}></AddTeamPopup>
+      </div>
       <div class="my-teams-container">
         <div class="my-teams-container-container">
+          <div onClick={handleAddCard}>
+            <AddTeamCard></AddTeamCard>
+          </div>
           {teams.map((item) => {
             return <SimpleCard title={item.name} id={item.id}></SimpleCard>;
           })}
--- src/components/teamsCard/TeamsCard.js
+++ src/components/teamsCard/TeamsCard.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
@@ -21,31 +19,27 @@ export default function ImgMediaCard(props) {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="300"
-          image={props.image}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            <LinkedInIcon></LinkedInIcon>
-          </Button>
-          <Button size="small" color="primary">
-            <GitHubIcon></GitHubIcon>
-          </Button>
-        </CardActions>
-      </CardActionArea>
+      <CardMedia
+        component="img"
+        alt="Contemplative Reptile"
+        height="300"
+        image={props.image}
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <Button size="small" color="primary">
+        <LinkedInIcon></LinkedInIcon>
+      </Button>
+      <Button size="small" color="primary">
+        <GitHubIcon></GitHubIcon>
+      </Button>
     </Card>
   );
 }
